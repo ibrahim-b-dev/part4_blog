d46c91b8e4ee78327f9247dc72d47d10b7057bc6
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-27 14:14:34 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sumOfLikes, blog) => {
+    return sumOfLikes + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes
 }
