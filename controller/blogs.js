e270f5bc42a9f9e891fd6af7ba0e9daabd9b4980
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-29 22:16:20 +0300

    4.20

--- controller/blogs.js
+++ controller/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization")
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "")
-  }
-  return null
-}
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
     username: 1,
@@ -46,7 +38,7 @@ blogsRouter.post("/", async (request, response) => {
     })
   }
 
-  const decodeToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodeToken = jwt.verify(request.token, process.env.SECRET)
   const user = await User.findById(decodeToken.id)
 
   const blog = new Blog({
